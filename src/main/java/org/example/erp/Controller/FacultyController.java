0eece6bd938eb2a33cd289bdc00cfbc98c156b69
Pratik Shirgire <pratikshirgire2004@gmail.com>
2026-08-12 21:23:09 +0530

    Update FacultyController.java
    cros origin added

--- src/main/java/org/example/erp/Controller/FacultyController.java
+++ src/main/java/org/example/erp/Controller/FacultyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://enterprise-resourse-planning-erp.onrender.com")
 @RestController
 @RequestMapping("/faculty")
 public class FacultyController {
